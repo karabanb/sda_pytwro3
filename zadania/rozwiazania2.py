10c7b6897e48350f4a39b6114472b1d18891e12e
karabanb <bkarabands@gmail.com>
2019-06-16 11:13:42 +0200

    dodanie rozwizan

---
+++ zadania/rozwiazania2.py
@@ -0,0 +1,24 @@
+
+# Argumenty pozycyjne i nazwane funkcji
+
+# Zadanie 1
+# Napisz funkcję,która przyjmuję numer telefonu oraz opcjonalnie numer kierunkowy(+48) i flagę czy wyświetlić kierunkowy
+# a następnie wyświetla numer w formacie: `+48 123456789` lub `123456789`.
+
+
+def show_telnumber(number, prefix=False):
+    if prefix:
+        print(f'+48 {number}')
+    else:
+        print(number)
+
+
+# Zadanie 2
+# Uruchom funkcję z różną liczbą argumentów pozycyjnych (tylko numer, numer i kierunkowy, numer i kierunkowy oraz flaga)
+
+# Zadanie 3
+# Uruchom funkcję podając różną liczbę argumenty nazwanych.
+
+# Zadanie 4
+# Uruchom funkcję używając argumentów nazwanych i pozycyjnych.
+
