b53c03a782f5229ed94d1b0eea4b0e7b400ce030
karabanb <bkarabands@gmail.com>
2019-06-16 14:03:08 +0200

    dodanie rozwizan

--- zadania/rozwiazania4.py
+++ zadania/rozwiazania4.py
@@ -5,6 +5,17 @@
 # Rozpakuj listę argumentów i wywołaj funkcję z poprzedniego zadania.
 
 
+def foo3(*args, **kwargs):
+    res = 1
+    for value in kwargs.values():
+        res = res * value
+    return res, sum(args)
+
+
+l = [1, 2, 3]
+
+
+foo3(*l)
 
 
 # Zadanie 2
